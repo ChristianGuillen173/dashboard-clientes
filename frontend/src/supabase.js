8e5f8763079fb3845e88adf9bffedb4de491f7ba
Christian <guillenchristian173@gmail.com>
2026-04-02 13:06:54 -0300

    atualização segurança .env

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,8 +1,7 @@
 // src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wfrklnrncjswqzwkocfj.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmcmtsbnJuY2pzd3F6d2tvY2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNDE3MjksImV4cCI6MjA5MDcxNzcyOX0.Gy5cyKqZ-EpA0YQEFajtLCTnaGaaBB2JQJGsEZ_rUxk";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
